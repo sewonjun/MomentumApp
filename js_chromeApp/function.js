455808589e356f789e6c5574f79baa4a7a175e0f
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-03-24 02:47:56 +0900

    remember username using localstorage

--- js_chromeApp/function.js
+++ js_chromeApp/function.js
@@ -10,8 +10,12 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(UserNameKey, username)
-    greeting.innerText =`Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
@@ -22,6 +26,6 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    paintGreetings(savedUsername);
 }
