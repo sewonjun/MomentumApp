03c91a17a4a30f4c639835dba32e60d38affec27
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-03-24 02:09:31 +0900

    second

--- js_chromeApp/function.js
+++ js_chromeApp/function.js
@@ -1,13 +1,27 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"
+const UserNameKey = "username"
+
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(UserNameKey, username)
     greeting.innerText =`Hello ${username}`;
-    greeting.classList.remove("hidden")
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(UserNameKey)
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
+}
