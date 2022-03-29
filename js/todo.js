0ef304ab05b595dc39b08e5a6e70aaaec8360eb8
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-03-29 23:23:39 +0900

    delete todoList

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,23 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+function deleteToDo(event){
+    console.log(event)
+}
+
 function paintToDo(newToDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText="❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     todoList.appendChild(li);
 }
+
+
 
 function handleToDoSubmit(event){
     event.preventDefault();
