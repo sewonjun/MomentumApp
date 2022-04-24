ce7900a0aa9cce6a238d4a99781a45c12e9ff3f7
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-04-24 17:35:57 +0900

    finalPJ loign

---
+++ final_project/src/login.js
@@ -0,0 +1,22 @@
+const loginForm = document.querySelector("#login-form");
+const nameInput = document.querySelector("#login-form input:first-child");
+const loginBtn = document.querySelector("#login-form input:last-child");
+const namePaint = document.querySelector("#greeting");
+const HIDDEN_CLASS = "hidden";
+
+function loginSubmit (event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASS);
+    const username = nameInput.value;
+    localStorage.setItem("username", username);
+    paintGreeting(username);
+}
+function paintGreeting(username){
+    namePaint.innerText=`Hello ${username}`;
+    namePaint.classList.remove(HIDDEN_CLASS);
+}
+
+const savedUsername = localStorage.getItem("username");
+
+loginForm.addEventListener("submit", loginSubmit );
+
