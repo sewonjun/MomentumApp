3072619dec4966ef78aa093b14c8c6229b0100b6
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-05-04 17:20:04 +0900

    login validation fin

--- final_project/js/login.js
+++ final_project/js/login.js
@@ -6,8 +6,12 @@ const HIDDEN_CLASS = "hidden";
 
 function loginSubmit (event){
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASS);
     const username = nameInput.value;
+    if(username == ""){
+        alert("Name must be filled out");
+        return false;
+    }
+    loginForm.classList.add(HIDDEN_CLASS);
     localStorage.setItem("username", username);
     paintGreeting(username);
 }
@@ -15,25 +19,14 @@ function paintGreeting(username){
     namePaint.innerText=`Hello ${username}`;
     namePaint.classList.remove(HIDDEN_CLASS);
 }
-function formValidate(){
-    const username = nameInput.value;
-    if (username == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-const savedUsername = localStorage.getItem("username");
 
-if (formValidate == false) {
-    alert("Name must be filled out");
-} else {
-    paintGreeting(savedUsername);
-}
+const savedUsername = localStorage.getItem("username");
 
 if (savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASS);
     loginForm.addEventListener("submit", loginSubmit);
 } else {
+    loginForm.classList.add(HIDDEN_CLASS);
     paintGreeting(savedUsername);
 }
 
