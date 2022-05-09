704e782717e8db94c154aeed649ec3dff81a4c38
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-05-09 23:49:01 +0900

    commit0509

--- final_project/js/dDay.js
+++ final_project/js/dDay.js
@@ -1,8 +1,15 @@
-const dDay = document.querySelector("dDay");
+const dDayInput = document.querySelector("#dDay");
+const dDaySubmit = document.querySelector("#dDayBtn")
+const dDayForm = document.querySelector("#section2")
 //일단 input을 이용해서 dayp picker를 만들어 .. 그리고 그 다음에 그 value를 어떻게 넣는지 알아봐 
-function getDay(){
-    const Dday = document.querySelector(".dDay").value ;
-    const goalDay = new Date(Dday)
-    console.log( goalDay);
+function getDay(event){
+    event.preventDefault();
+    const realDay = dDayInput.value ;
+    localStorage.setItem("Dday", realDay); 
+    // const goalDay = new Date(Dday)
+    console.log(realDay);
 }
+
+dDayForm.addEventListener("submit", getDay);
+
 
--- final_project/js/login.js
+++ final_project/js/login.js
@@ -10,6 +10,8 @@ function loginSubmit (event){
     if(username == ""){
         alert("Name must be filled out");
         return false;
+        // return flase를 지우면 무슨 일이 일어나는가?? 그럼 그냥 username이 빈칸으로 제출된다
+        // 그렇다면 return false는 무엇을 말하는가? submit false를 말한다. 제출 못하게 하는 장치라는 뜻~~
     }
     loginForm.classList.add(HIDDEN_CLASS);
     localStorage.setItem("username", username);
