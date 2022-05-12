64bc6e73146e9e0cf1d3b5f1e24627d3938804e2
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-05-12 17:49:47 +0900

    how can i reload paintLeftdays

--- final_project/js/dDay.js
+++ final_project/js/dDay.js
@@ -2,10 +2,21 @@ const dDayInput = document.querySelector("#dDay");
 const dDaySubmit = document.querySelector("#dDayBtn");
 const dDayForm = document.querySelector("#dDayForm");
 const dDayPaint = document.querySelector("#paintdDay");
+const todayPaint = document.querySelector("#paintToday");
+const HIDDEN = "hidden";
 
 function paintLeftdays(){
     const savedDiffDay = localStorage.getItem("leftDays");
-    dDayPaint.innerText= `untill Dday ${savedDiffDay}`;
+    const savedToday = localStorage.getItem("today");
+    if(savedDiffDay === null || savedToday===null){
+        alert("insert dayPicker");
+    } else {
+        dDayForm.classList.add(HIDDEN);
+        dDayPaint.classList.remove(HIDDEN);
+        todayPaint.classList.remove(HIDDEN);
+        todayPaint.innerText = `Today is ${savedToday}`;
+        dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+    }
 }
 
 function getDay(event){
@@ -16,15 +27,18 @@ function getDay(event){
     console.log(goalDay);
     // const savedDay = localStorage.getItem("Dday");
     const today = new Date();
-    console.log(today);
+    const simpleToday = new Date().toISOString().slice(0, 10);
+    console.log(simpleToday)
+    // console.log(today);
+    localStorage.setItem("today", simpleToday);
     const diff = goalDay - today;
     console.log(diff);
     const getDiffDay = Math.ceil(diff/(1000*60*60*24));
     console.log(getDiffDay);
     localStorage.setItem("leftDays", getDiffDay);
-    paintLeftdays();
 }
 
 dDayForm.addEventListener("submit", getDay);
+paintLeftdays();
 
 
