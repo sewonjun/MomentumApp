5e451b3986944a697abb043345bba2955048f2fc
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-05-13 17:38:08 +0900

    fin

--- final_project/js/dDay.js
+++ final_project/js/dDay.js
@@ -5,7 +5,14 @@ const dDayPaint = document.querySelector("#paintdDay");
 const todayPaint = document.querySelector("#paintToday");
 const HIDDEN = "hidden";
 
-function paintLeftdays(){
+function paintLeftDays(){
+    const savedDiffDay = localStorage.getItem("leftDays");
+    const savedToday = localStorage.getItem("today");
+    todayPaint.innerText = `Today is ${savedToday}`;
+    dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+}
+
+function nullCheck(){
     const savedDiffDay = localStorage.getItem("leftDays");
     const savedToday = localStorage.getItem("today");
     if(savedDiffDay === null || savedToday===null){
@@ -14,8 +21,9 @@ function paintLeftdays(){
         dDayForm.classList.add(HIDDEN);
         dDayPaint.classList.remove(HIDDEN);
         todayPaint.classList.remove(HIDDEN);
-        todayPaint.innerText = `Today is ${savedToday}`;
-        dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+        paintLeftDays();
+        // todayPaint.innerText = `Today is ${savedToday}`;
+        // dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
     }
 }
 
@@ -25,20 +33,22 @@ function getDay(event){
     localStorage.setItem("Dday", realDay); 
     const goalDay = new Date(realDay)
     console.log(goalDay);
-    // const savedDay = localStorage.getItem("Dday");
     const today = new Date();
     const simpleToday = new Date().toISOString().slice(0, 10);
     console.log(simpleToday)
-    // console.log(today);
     localStorage.setItem("today", simpleToday);
     const diff = goalDay - today;
     console.log(diff);
     const getDiffDay = Math.ceil(diff/(1000*60*60*24));
     console.log(getDiffDay);
     localStorage.setItem("leftDays", getDiffDay);
+    // const savedDiffDay = localStorage.getItem("leftDays");
+    // const savedToday = localStorage.getItem("today");
+    // todayPaint.innerText = `Today is ${savedToday}`;
+    // dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+    paintLeftDays();
 }
-
+nullCheck();
 dDayForm.addEventListener("submit", getDay);
-paintLeftdays();
 
 
