693924fde66bfe1834f8073ca5b31567a7fa20a8
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-05-13 21:45:28 +0900

    fin2

--- final_project/js/dDay.js
+++ final_project/js/dDay.js
@@ -3,27 +3,31 @@ const dDaySubmit = document.querySelector("#dDayBtn");
 const dDayForm = document.querySelector("#dDayForm");
 const dDayPaint = document.querySelector("#paintdDay");
 const todayPaint = document.querySelector("#paintToday");
+const resetBtn = document.querySelector("#resetBtn");
 const HIDDEN = "hidden";
 
-function paintLeftDays(){
-    const savedDiffDay = localStorage.getItem("leftDays");
-    const savedToday = localStorage.getItem("today");
-    todayPaint.innerText = `Today is ${savedToday}`;
-    dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
-}
+// function paintLeftDays(){
+//     const savedDiffDay = localStorage.getItem("leftDays");
+//     const savedToday = localStorage.getItem("today");
+//     todayPaint.innerText = `Today is ${savedToday}`;
+//     dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+// }
 
 function nullCheck(){
     const savedDiffDay = localStorage.getItem("leftDays");
     const savedToday = localStorage.getItem("today");
-    if(savedDiffDay === null || savedToday===null){
+    const savedleftDays = localStorage.getItem("leftDays")
+    if(savedDiffDay === null || savedToday === null || savedleftDays === NaN){
         alert("insert dayPicker");
+        dDayForm.classList.remove(HIDDEN);
+        dDayPaint.classList.add(HIDDEN);
+        todayPaint.classList.add(HIDDEN);
     } else {
         dDayForm.classList.add(HIDDEN);
         dDayPaint.classList.remove(HIDDEN);
         todayPaint.classList.remove(HIDDEN);
-        paintLeftDays();
-        // todayPaint.innerText = `Today is ${savedToday}`;
-        // dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+        todayPaint.innerText = `Today is ${savedToday}`;
+        dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
     }
 }
 
@@ -42,13 +46,23 @@ function getDay(event){
     const getDiffDay = Math.ceil(diff/(1000*60*60*24));
     console.log(getDiffDay);
     localStorage.setItem("leftDays", getDiffDay);
-    // const savedDiffDay = localStorage.getItem("leftDays");
-    // const savedToday = localStorage.getItem("today");
-    // todayPaint.innerText = `Today is ${savedToday}`;
-    // dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
-    paintLeftDays();
+    const savedDiffDay = localStorage.getItem("leftDays");
+    const savedToday = localStorage.getItem("today");
+    todayPaint.innerText = `Today is ${savedToday}`;
+    dDayPaint.innerText = `D-DAY ${savedDiffDay}`;
+    dDayForm.classList.add(HIDDEN);
+    dDayPaint.classList.remove(HIDDEN);
+    todayPaint.classList.remove(HIDDEN);
+
 }
 nullCheck();
 dDayForm.addEventListener("submit", getDay);
+resetBtn.addEventListener("submit", resetDday);
 
-
+function resetDday(event){
+    event.preventDefault();
+    localStorage.removeItem("leftDays");
+    return (
+        nullCheck()
+    )
+}
